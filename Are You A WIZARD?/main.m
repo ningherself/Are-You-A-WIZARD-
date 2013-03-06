66e58e001560ace97798b27bd3c1000ac0c36813
yining <yininglim@seconds-MacBook-Air.local>
2013-03-07 00:51:56 +0800

    Initial Commit

---
+++ Are You A WIZARD?/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Are You A WIZARD?
+//
+//  Created by yining on 3/7/13.
+//  Copyright (c) 2013 yining. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
